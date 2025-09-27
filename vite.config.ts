b1c4b5d516d2464156ae0adfc584f94fcf6d21f6
alexisborgesm-oss <alexisborgesm@gmail.com>
2025-09-26 18:53:55 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,17 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+           {
+      name: 'html-transform',
+      transformIndexHtml(html) {
+        return html.replace(/<title>(.*?)<\/title>/, '<title>Inventory</title>')
+      }],
   server: {
     allowedHosts: [
       "localhost",
       "127.0.0.1",
+      "https://inventory-deploy-sable.vercel.app",
       "5zxjm6-5173.csb.app", // 👈 tu dominio público en CodeSandbox
     ],
   },
